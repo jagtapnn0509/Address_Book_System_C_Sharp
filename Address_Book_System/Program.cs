ffd0a9c8ed8a6faf2180267d8bbb15a3c2a1bf67
jagtapnn0509 <jagtapnn05@gmail.com>
2021-05-12 23:50:06 +0530

    Add : Dictionary to create multiple address book

--- Address_Book_System/Program.cs
+++ Address_Book_System/Program.cs
@@ -5,7 +5,80 @@ namespace Address_Book_System
 {
     class Program
     {
-        public void Create_Contacts(Data contact)
+        public static Dictionary<string, AdddressBookBuilder> AddressBookDictionary = new Dictionary<string, AdddressBookBuilder>();
+        public static void FillAddressBook(AdddressBookBuilder addressbook)
+        {
+            int choice = 1;
+
+            while (choice != 5)
+            {
+                Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                Console.WriteLine("Menu : \n 1.Add Contact \n 2.Display Contact \n 3.Update Contact Using First Name \n 4.Delete Contact using first Name \n 5.Exit ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                switch (choice)
+                {
+                    case 1:
+                        Data contact = new Data();
+                        Create_Contacts(contact);
+                        addressbook.AddContact(contact);
+                        Console.WriteLine("Contact Added Successfully.");
+                        break;
+                    case 2:
+                        addressbook.Display_Contacts();
+                        break;
+                    case 3:
+                        Console.WriteLine("which contact you want to update Enter First Name");
+                        String FirstName = (Console.ReadLine());
+                        int index = addressbook.Update_Contact(FirstName);
+                        Console.WriteLine("Index is " + index);
+                        if (index == -1)
+                        {
+                            Console.WriteLine("Contact Not Found");
+                        }
+                        else
+                        {
+                            Data contact2 = new Data();
+                            Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                            Console.WriteLine("-----------Before Updating-----------");
+                            Console.WriteLine($"First Name :{addressbook.list[index].First_Name} \nLast Name : {addressbook.list[index].Last_Name} \nAddress : {addressbook.list[index].Address} \nCity : {addressbook.list[index].City} \nState : {addressbook.list[index].State} \nZip : {addressbook.list[index].Zip} \nPhone No : {addressbook.list[index].Phone_No} \nEmail : {addressbook.list[index].Email}");
+                            Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                            Create_Contacts(contact2);
+                            addressbook.list[index] = contact2;
+                            Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                            Console.WriteLine("-----------After Updating---------");
+                            Console.WriteLine($"First Name :{addressbook.list[index].First_Name} \nLast Name : {addressbook.list[index].Last_Name} \nAddress : {addressbook.list[index].Address} \nCity : {addressbook.list[index].City} \nState : {addressbook.list[index].State} \nZip : {addressbook.list[index].Zip} \nPhone No : {addressbook.list[index].Phone_No} \nEmail : {addressbook.list[index].Email}");
+                            Console.WriteLine("Contact Update Successfully.");
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("which contact you want to Delete Enter First Name");
+                        String First_Name = (Console.ReadLine());
+                        int index1 = addressbook.Update_Contact(First_Name);
+                        Console.WriteLine("Index is " + index1);
+                        if (index1 == -1)
+                        {
+                            Console.WriteLine("Contact Not Found");
+                        }
+                        else
+                        {
+                            Data contact2 = new Data();
+                            addressbook.list.RemoveAt(index1);
+                            Console.WriteLine("Contact Delete Successfully.");
+                        }
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Program Exited");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input");
+                        break;
+                }
+
+            }
+        }
+        public static void Create_Contacts(Data contact)
         {
             Console.Write("Enter the First Name : ");
             contact.First_Name = (Console.ReadLine());
@@ -34,76 +107,40 @@ namespace Address_Book_System
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
-            AdddressBookBuilder addressbook = new AdddressBookBuilder();
-            Program addressbookobj = new Program();
+
             int choice = 1;
-            while (choice != 5)
+
+            while(choice != 3)
             {
                 Console.WriteLine("---------------------------------------------------------------------------------------------------");
-                Console.WriteLine("Menu : \n 1.Add Contact \n 2.Display Contact \n 3.Update Contact Using First Name \n 4.Delete Contact using first Name \n 5.Exit ");
+                Console.WriteLine("Menu : \n 1.Add new Address Book name \n 2.Work in existing address book \n 3.exit ");
                 choice = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("---------------------------------------------------------------------------------------------------");
                 switch (choice)
                 {
                     case 1:
-                        Data contact = new Data();
-                        addressbookobj.Create_Contacts(contact);
-                        addressbook.AddContact(contact);
-                        Console.WriteLine("Contact Added Successfully.");
-                        break;
-                    case 2:
-                        addressbook.Display_Contacts();
-                        break;
-                    case 3:
-                        Console.WriteLine("which contact you want to update Enter First Name");
-                        String FirstName = (Console.ReadLine());
-                        int index = addressbook.Update_Contact(FirstName);
-                        Console.WriteLine("Index is "+ index);
-                        if (index == -1)
-                        {
-                            Console.WriteLine("Contact Not Found");
-                        }
-                        else
-                        {
-                            Data contact2 = new Data();
-                            Console.WriteLine("---------------------------------------------------------------------------------------------------");
-                            Console.WriteLine("-----------Before Updating-----------");
-                            Console.WriteLine($"First Name :{addressbook.list[index].First_Name} \nLast Name : {addressbook.list[index].Last_Name} \nAddress : {addressbook.list[index].Address} \nCity : {addressbook.list[index].City} \nState : {addressbook.list[index].State} \nZip : {addressbook.list[index].Zip} \nPhone No : {addressbook.list[index].Phone_No} \nEmail : {addressbook.list[index].Email}"); 
-                            Console.WriteLine("---------------------------------------------------------------------------------------------------");
-                            addressbookobj.Create_Contacts(contact2);
-                            addressbook.list[index] = contact2;
-                            Console.WriteLine("---------------------------------------------------------------------------------------------------");
-                            Console.WriteLine("-----------After Updating---------");
-                            Console.WriteLine($"First Name :{addressbook.list[index].First_Name} \nLast Name : {addressbook.list[index].Last_Name} \nAddress : {addressbook.list[index].Address} \nCity : {addressbook.list[index].City} \nState : {addressbook.list[index].State} \nZip : {addressbook.list[index].Zip} \nPhone No : {addressbook.list[index].Phone_No} \nEmail : {addressbook.list[index].Email}");
-                            Console.WriteLine("Contact Update Successfully.");
-                        }
-                        break;
-                    case 4:
-                        Console.WriteLine("which contact you want to Delete Enter First Name");
-                        String First_Name = (Console.ReadLine());
-                        int index1 = addressbook.Update_Contact(First_Name);
-                        Console.WriteLine("Index is " + index1);
-                        if (index1 == -1)
-                        {
-                            Console.WriteLine("Contact Not Found");
-                        }
-                        else
-                        {
-                            Data contact2 = new Data();
-                            addressbook.list.RemoveAt(index1);
-                            Console.WriteLine("Contact Delete Successfully.");
-                        }    
+                        Console.Write("Enter New address book name : ");
+                        String Name = (Console.ReadLine());
+                        AddressBookDictionary.Add(Name, new AdddressBookBuilder());
                         break;
 
-                    case 5:
-                        Console.WriteLine("Program Exited");
+                    case 2:
+                        Console.Write("In which address book you want to work : ");
+                        String name = (Console.ReadLine());
+                        AdddressBookBuilder addressbook = AddressBookDictionary[name];
+                        FillAddressBook(addressbook);
                         break;
+
+                    case 3:
+                        Console.WriteLine("Program is terminated");
+                        break;
+
                     default:
-                        Console.WriteLine("Wrong input");
+                        Console.WriteLine("Your input is wrong");
                         break;
-                }
-               
+                }    
             }
+           
         }
     }
 }
